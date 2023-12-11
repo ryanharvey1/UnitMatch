bd8b2e32250e846aeb9f3defa6c29a5e8686875a
Ryan Harvey <ryanh412@gmail.com>
2023-12-11 14:42:59 -0500

    cell explorer spikes

--- UnitMatchPipeline/ExtractAndSaveAverageWaveforms.m
+++ UnitMatchPipeline/ExtractAndSaveAverageWaveforms.m
@@ -1,4 +1,5 @@
-function Path4UnitNPY = ExtractAndSaveAverageWaveforms(clusinfo,param)
+function Path4UnitNPY = ExtractAndSaveAverageWaveforms(clusinfo, param)
+
 %% Called by UnitMatch, but can also be used on its own to save two averaged waveforms per unit per session
 
 %% Read in from param
@@ -7,6 +8,7 @@ AllDecompPaths = param.AllDecompPaths;
 sampleamount = param.sampleamount; %500; % Nr. waveforms to include
 spikeWidth = param.spikeWidth; %83; % in sample space (time)
 halfWidth = floor(spikeWidth/2);
+
 %% Extract all cluster info
 AllClusterIDs = clusinfo.cluster_id;
 if param.GoodUnitsOnly
@@ -24,83 +26,120 @@ nclus = length(Good_Idx);
 dataTypeNBytes = numel(typecast(cast(0, 'uint16'), 'uint8')); % Define datatype
 
 % Initialize
-Path4UnitNPY = cell(1,nclus);
+Path4UnitNPY = cell(1, nclus);
+
+load(fullfile(param.basepaths{1}, [basenameFromBasepath(param.basepaths{1}), '.session.mat']), 'session')
+channel_idx = [session.extracellular.electrodeGroups.channels{:}];
+
 
 timercounter = tic;
-fprintf(1,'Extracting raw waveforms. Progress: %3d%%',0)
+fprintf(1, 'Extracting raw waveforms. Progress: %3d%%', 0)
+
+% load spikes for all sessions
+nfiles = length(param.basepaths);
+spAll = cell(1, nfiles);
+for basepath_i = 1:nfiles
+    basepath = param.basepaths{basepath_i};
+    load(fullfile(basepath, [basenameFromBasepath(basepath), '.spikes.cellinfo.mat']), 'spikes')
+    % spikes,st = importSpikes('basepath',basepath)
+    spAll{basepath_i}.st = spikes.spindices(:, 1);
+    spAll{basepath_i}.spikeTemplates = spikes.spindices(:, 2);
+    spAll{basepath_i}.RecSesID = ones(length(spikes.spindices(:, 1)), 1) * basepath_i;
+    spAll{basepath_i}.sample_rate = spikes.sr;
+end
+% Add all spikedata in one spikes struct - can be used for further analysis
+spAll = [spAll{:}];
+spnew = struct;
+fields = fieldnames(spAll(1));
+for fieldid = 1:length(fields)
+    try
+        spnew.(fields{fieldid}) = cat(1, spAll(:).(fields{fieldid}));
+    catch ME
+        if strcmp(ME.message, 'Out of memory.')
+            spnew.(fields{fieldid}) = cat(1, spAll(1).(fields{fieldid}));
+            for tmpid = 2:length(spAll)
+                spnew.(fields{fieldid}) = cat(1, spnew.(fields{fieldid}), spAll(tmpid).(fields{fieldid}));
+            end
+        else
+            spnew.(fields{fieldid}) = cat(1, spAll(:).(fields{fieldid}));
+            eval(['spnew.', fields{fieldid}, '= cat(2,sp(:).', fields{fieldid}, ');'])
+        end
+    end
+end
+sp = spnew;
+sp.sample_rate = sp.sample_rate(1);
+clear spikes
+
+
 Currentlyloaded = 0;
 for uid = 1:nclus
-    fprintf(1,'\b\b\b\b%3.0f%%',uid/nclus*100)
-    if length(param.KSDir)>1
-        tmppath = dir(fullfile(param.KSDir{GoodRecSesID(uid)},'**','RawWaveforms*'));
-    else %Stitched KS
-        tmppath = dir(fullfile(param.KSDir{1},'**','RawWaveforms*'));
+    fprintf(1, '\b\b\b\b%3.0f%%', uid/nclus*100)
+    % if length(param.KSDir)>1
+    % tmppath = dir(fullfile(param.KSDir{GoodRecSesID(uid)},'**','RawWaveforms*'));
+    % tmppath = dir(fullfile(param.KSDir{GoodRecSesID(uid)}, 'RawWaveforms'));
+    if ~exist(fullfile(param.KSDir{GoodRecSesID(uid)}, 'RawWaveforms'), 'dir')
+        mkdir(fullfile(param.KSDir{GoodRecSesID(uid)}, 'RawWaveforms'))
     end
-    if length(tmppath)>1
-        % Probably stitched:
-        tmppath = tmppath(GoodRecSesID(uid));
-    end
-    Path4UnitNPY{uid} = fullfile(tmppath.folder,tmppath.name,['Unit' num2str(AllClusterIDs(Good_Idx(uid))) '_RawSpikes.npy']); %0-indexed
+
+    % else %Stitched KS
+    %     tmppath = dir(fullfile(param.KSDir{1},'**','RawWaveforms*'));
+    % end
+    % if length(tmppath) > 1
+    %     % Probably stitched:
+    %     tmppath = tmppath(GoodRecSesID(uid));
+    % end
+    Path4UnitNPY{uid} = fullfile(fullfile(param.KSDir{GoodRecSesID(uid)}), 'RawWaveforms', ['Unit', num2str(AllClusterIDs(Good_Idx(uid))), '_RawSpikes.npy']); %0-indexed
 
     if exist(Path4UnitNPY{uid}) && ~RedoExtraction
         continue
-    else       
+    else
         if ~(GoodRecSesID(uid) == Currentlyloaded) % Only load new memmap if not already loaded
 
             % Map the data
-            clear memMapData
+            % clear memMapData
             spikeFile = dir(AllDecompPaths{GoodRecSesID(uid)});
-            try %hacky way of figuring out if sync channel present or not
-                n_samples = spikeFile.bytes / (param.nChannels * dataTypeNBytes);
-                nChannels = param.nChannels - 1; % Last channel is sync, ignore for now
-                ap_data = memmapfile(AllDecompPaths{GoodRecSesID(uid)}, 'Format', {'int16', [param.nChannels, n_samples], 'data'});
-            catch
-                nChannels = param.nChannels - 1;
-                n_samples = spikeFile.bytes / (param.nChannels * dataTypeNBytes);
-                ap_data = memmapfile(AllDecompPaths{GoodRecSesID(uid)}, 'Format', {'int16', [nChannels, n_samples], 'data'});
-            end
-            memMapData = ap_data.Data.data;
+
+            n_samples = spikeFile.bytes / (param.nChannels * dataTypeNBytes);
+            ap_data = memmapfile(AllDecompPaths{GoodRecSesID(uid)}, 'Format', {'int16', [param.nChannels, n_samples], 'data'});
+
+            % memMapData = ap_data.Data.data;
             Currentlyloaded = GoodRecSesID(uid);
         end
 
-        %load sp
-        if length(param.KSDir)>1
-            tmp = matfile(fullfile(param.KSDir{GoodRecSesID(uid)},'PreparedData.mat'));
-        else %Stitched Kilosort
-            tmp = matfile(fullfile(param.KSDir{1},'PreparedData.mat'));
-        end
-        sp = tmp.sp;
-        tmpclusinfo = tmp.clusinfo; %Load original clusinfo
-
         % Spike samples
-        idx1=(sp.st(sp.spikeTemplates == AllClusterIDs(Good_Idx(uid)) & sp.SessionID == tmpclusinfo.RecSesID(Good_Idx(uid))).*round(sp.sample_rate));  % Spike times in samples;
+        idx1 = (sp.st(sp.spikeTemplates == AllClusterIDs(Good_Idx(uid)) & ...
+            sp.RecSesID == clusinfo.RecSesID(Good_Idx(uid))) .* round(sp.sample_rate)); % Spike times in samples;
 
         %Extract raw waveforms on the fly - % Unit uid
-        if sampleamount<length(idx1)
-            spikeIndicestmp = sort(datasample(idx1,sampleamount,'replace',false));
+        if sampleamount < length(idx1)
+            spikeIndicestmp = sort(datasample(idx1, sampleamount, 'replace', false));
         else
             spikeIndicestmp = sort(idx1);
         end
-        spikeMap = nan(spikeWidth,nChannels,length(spikeIndicestmp));
+        spikeMap = nan(spikeWidth, param.nChannels, length(spikeIndicestmp));
         for iSpike = 1:length(spikeIndicestmp)
             thisSpikeIdx = int32(spikeIndicestmp(iSpike));
-            if thisSpikeIdx > halfWidth && (thisSpikeIdx + halfWidth) < size(memMapData,2) % check that it's not out of bounds
-                tmp = smoothdata(double(memMapData(1:nChannels,thisSpikeIdx-halfWidth:thisSpikeIdx+halfWidth)),2,'gaussian',5);
-                tmp = (tmp - mean(tmp(:,1:20),2))';
-                tmp(:,end+1:nChannels) = nan(size(tmp,1),nChannels-size(tmp,2));
+            if thisSpikeIdx > halfWidth && (thisSpikeIdx + halfWidth) < size(ap_data.Data.data, 2) % check that it's not out of bounds
+
+                tmp = smoothdata(double(ap_data.Data.data(channel_idx, thisSpikeIdx-halfWidth:thisSpikeIdx+halfWidth)), ...
+                    floor(0.000066*sp.sample_rate), 'gaussian', floor(0.00016*sp.sample_rate));
+
+                tmp = (tmp - mean(tmp(:, 1:floor(0.00066*sp.sample_rate)), 2))';
+
+                tmp(:, end+1:param.nChannels) = nan(size(tmp, 1), param.nChannels-size(tmp, 2));
                 % Subtract first 10 samples to level spikes
-                spikeMap(:,:,iSpike) = tmp(1:spikeWidth,:);
+                spikeMap(:, :, iSpike) = tmp(1:spikeWidth, :);
             end
         end
         %Actual number of wavefroms
-        nwavs = sum(sum(~isnan(nanmean(spikeMap,2)),1) == spikeWidth); % Actual number of waves
+        nwavs = sum(sum(~isnan(nanmean(spikeMap, 2)), 1) == spikeWidth); % Actual number of waves
         for cv = 1:2
-            if cv==1
+            if cv == 1
                 wavidx = floor(1:nwavs/2);
             else
                 wavidx = floor(nwavs/2+1:nwavs);
             end
-            spikeMapAvg(:,:,cv) = nanmedian(spikeMap(:,:,wavidx),3);
+            spikeMapAvg(:, :, cv) = nanmedian(spikeMap(:, :, wavidx), 3);
         end
         spikeMap = spikeMapAvg;
         clear spikeMapAvg
@@ -110,4 +149,4 @@ for uid = 1:nclus
 end
 
 fprintf('\n')
-disp(['Extracting raw waveforms took ' num2str(round(toc(timercounter)./60)) ' minutes for ' num2str(nclus) ' units'])
+disp(['Extracting raw waveforms took ', num2str(round(toc(timercounter)./60)), ' minutes for ', num2str(nclus), ' units'])
